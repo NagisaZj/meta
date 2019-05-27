4db21cf5dd9bde2f62071ccd6ad313c65a0af505
jin zhang <1170863106@qq.com>
2019-05-27 16:08:17 +0800

    fff

---
+++ test/linear_toy.py
@@ -0,0 +1,121 @@
+import numpy as np
+import torch
+import argparse
+import os
+import torch.nn as nn
+from collections import OrderedDict
+import torch.nn.functional as F
+from torch.nn.utils.convert_parameters import (vector_to_parameters,
+                                               parameters_to_vector)
+import matplotlib.pyplot as plt
+
+def weight_init(module):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight)
+        #module.bias.data.zero_()
+
+def weight_init_bias(module):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight)
+        module.bias.data.zero_()
+
+class network(nn.Module):
+    def __init__(self,num_b,allow_bias):
+        super(network,self).__init__()
+        for i in range(num_b):
+            self.add_module('layer{0}'.format(i),nn.Linear(1,1,bias=False))
+        if allow_bias:
+            self.apply(weight_init_bias)
+        else:
+            self.apply(weight_init)
+        self.num_b = num_b
+        self.allow_bias = allow_bias
+
+    def forward(self, input, params=None):
+        if params is None:
+            params = OrderedDict(self.named_parameters())
+        output = input
+        for i in range(self.num_b):
+            output = F.linear(output,
+                              weight=params['layer{0}.weight'.format(i)],
+                              bias=torch.zeros(1,) if not self.allow_bias else params['layer{0}.bias'.format(i)])
+
+        return output
+
+    def update_params(self, loss, step_size=0.5, first_order=False):
+        """Apply one step of gradient descent on the loss function `loss`, with
+        step-size `step_size`, and returns the updated parameters of the neural
+        network.
+        """
+        grads = torch.autograd.grad(loss, self.parameters(),
+                                    create_graph=not first_order)
+        updated_params = OrderedDict()
+        for (name, param), grad in zip(self.named_parameters(), grads):
+            updated_params[name] = param - step_size * grad
+        return updated_params
+
+def learn(learner,args,task_target,log_dir):
+    if args.num_b==1:
+        for update in range(args.num_updates):
+            input = torch.FloatTensor(torch.ones(1, 1))
+            lossfun = nn.MSELoss()
+            losses = []
+            loss_rem = []
+            b_rem = []
+            b_after_rem = []
+            for i in range(args.num_tasks):
+                b_after_rem.append([])
+            for i in range(args.num_tasks):
+                target = torch.FloatTensor(torch.ones(1, 1) * task_target[i])
+                loss = lossfun(learner.forward(input), target)
+                params = learner.update_params(loss, step_size=args.inner_lr, first_order=args.first_order)
+                b_after_rem[i].append(params['layer0.weight'].data.numpy()[0, 0])
+                losses.append(lossfun(learner.forward(input, params), target))
+
+            total_loss = torch.mean(torch.stack(losses, dim=0))
+            loss_rem.append(total_loss.data.numpy())
+            grads = torch.autograd.grad(total_loss, learner.parameters())
+            grads = parameters_to_vector(grads)
+            old_params = parameters_to_vector(learner.parameters())
+            vector_to_parameters(old_params - args.outer_lr * grads, learner.parameters())
+            b_rem.append(learner.layer0.weight.data[0, 0].numpy())
+
+    return
+
+
+
+def main(args,log_dir):
+    learner = network(args.num_b,args.allow_bias)
+    task_target = [args.B1, args.B2, args.B3]
+    learn(learner,args,task_target,log_dir)
+        #save(args)
+
+    return
+
+
+
+
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_updates', help='number of updates', type=int, default=200)
+    parser.add_argument('--num_b', help='number of network parameters b', type=int, default=1)
+    parser.add_argument('--B1',help='the target of regression task 1',type=int,default=3)
+    parser.add_argument('--B2', help='the target of regression task 2', type=int, default=5)
+    parser.add_argument('--B3', help='the target of regression task 3', type=int, default=7)
+    parser.add_argument('--num_tasks', help='number of regression tasks', type=int, default=2)
+    parser.add_argument('--inner_lr', help='lr for inner update', type=float, default=0.1)
+    parser.add_argument('--outer_lr', help='lr for outer update', type=float, default=0.1)
+    parser.add_argument('--first_order', help='use first order approximation', action='store_true')
+    parser.add_argument('--plot', help='whether plot figure', action='store_true')
+    parser.add_argument('--allow_bias', help='allowing bias', action='store_true')
+    parser.add_argument('--dir', help='log directory', type=str,default='./data/linear_toy/')
+
+    args = parser.parse_args()
+    log_dir=args.dir+str(args.num_b)+'_'+str(args.num_tasks)+'_'+str(args.inner_lr)+'_'+str(
+        args.outer_lr)+'_'+str(args.B1)+'_'+str(args.B2)+'_'+str(args.B3)+'/'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    main(args,log_dir)
